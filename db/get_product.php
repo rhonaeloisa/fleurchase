979c5ed67837dbaa587f6d84e9edaa466b34306a
Hershey <hah2023-3276-22539@bicol-u.edu.ph>
2026-05-15 01:31:17 +0800

    for products inventory

---
+++ db/get_product.php
@@ -0,0 +1,21 @@
+<?php
+include 'connection_db.php';
+
+$id = $_GET['id'] ?? '';
+
+$sql = "SELECT * FROM product WHERE product_id = ?";
+$stmt = mysqli_prepare($conn, $sql);
+mysqli_stmt_bind_param($stmt, "i", $id);
+mysqli_stmt_execute($stmt);
+
+$result = mysqli_stmt_get_result($stmt);
+$row = mysqli_fetch_assoc($result);
+
+header('Content-Type: application/json');
+
+if ($row) {
+  echo json_encode($row);
+} else {
+  echo json_encode(["error" => "Product not found."]);
+}
+?>
