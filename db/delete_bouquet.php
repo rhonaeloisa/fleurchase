c87126cbe961cd0fe6d7f75dc03a427197680355
098-ayis <lobarbioayis19@gmail.com>
2026-05-16 18:26:48 +0800

    delete bouquet working

--- db/delete_bouquet.php
+++ db/delete_bouquet.php
@@ -1,21 +1,34 @@
 <?php
 include 'connection_db.php';
 
-$id = $_GET['id'] ?? '';
+$id = (int)($_GET['id'] ?? 0);
 
-if (!$id) {
+if ($id <= 0) {
   header("Location: ../products-admin.php");
   exit;
 }
 
-$sql = "DELETE FROM bouquet WHERE bouquet_id = ?";
-$stmt = mysqli_prepare($conn, $sql);
-mysqli_stmt_bind_param($stmt, "i", $id);
+mysqli_begin_transaction($conn);
 
-if (mysqli_stmt_execute($stmt)) {
+try {
+  // Delete child records first
+  $sqlChild = "DELETE FROM bouquet_product WHERE bouquet_id = ?";
+  $stmtChild = mysqli_prepare($conn, $sqlChild);
+  mysqli_stmt_bind_param($stmtChild, "i", $id);
+  mysqli_stmt_execute($stmtChild);
+
+  // Then delete bouquet
+  $sql = "DELETE FROM bouquet WHERE bouquet_id = ?";
+  $stmt = mysqli_prepare($conn, $sql);
+  mysqli_stmt_bind_param($stmt, "i", $id);
+  mysqli_stmt_execute($stmt);
+
+  mysqli_commit($conn);
+
   header("Location: ../products-admin.php");
   exit;
-} else {
-  echo "Error deleting bouquet: " . mysqli_error($conn);
+} catch (Throwable $e) {
+  mysqli_rollback($conn);
+  echo "Error deleting bouquet: " . $e->getMessage();
 }
 ?>
